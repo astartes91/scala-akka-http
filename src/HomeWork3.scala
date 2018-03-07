dbdc8bd382858260aa9c7d2d68b74d5b885cabc0
Vladimir Nizamutdinov <v.nizamutdinov@softage.ru>
2018-03-07 18:12:28 +0700

    findMinNegativeMaxPositive function added

--- src/HomeWork3.scala
+++ src/HomeWork3.scala
@@ -1,7 +1,7 @@
-
 object HomeWork3 {
   def main(args: Array[String]) {
     println(countSignChange(Seq(1, -1, 0, -2, -3)))
+    println(findMinNegativeMaxPositive(Seq(1, -1, 100, -200)))
   }
 
   def countSignChange(seq: Seq[Int]): Int =
@@ -13,4 +13,11 @@ object HomeWork3 {
         else 0)
         + countSignChange(tail))
     }
+
+  //todo:
+  def findMinNegativeMaxPositive(seq: Seq[Int]): (Option[Int], Option[Int]) = {
+    val negatives = seq.filter(number => number < 0)
+    val positives = seq.filter(number => number > 0)
+    (if(negatives.isEmpty) None else Some(negatives.min), if(positives.isEmpty) None else Some(positives.max))
+  }
 }
