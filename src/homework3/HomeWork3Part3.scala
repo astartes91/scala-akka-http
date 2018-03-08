d00fef2a9b0bcfec72ca9856c34e35e5d697906b
unknown <astartes91@gmail.com>
2018-03-08 21:26:44 +0700

    HomeWork 3 part 3 changed

--- src/homework3/HomeWork3Part3.scala
+++ src/homework3/HomeWork3Part3.scala
@@ -8,30 +8,35 @@ object HomeWork3Part3 {
   def main(args: Array[String]) {
 
     var seq: Seq[(Int) => Option[Int]] = Seq(div2IfNotOdd, add1IfOdd)
-    println(applyTransformations(10, seq))
+    println(applyTransformationsByReducing(10, seq))
+    println(applyTransformationsByPatternMatching(10, seq))
 
-    seq = Seq(add1IfOdd, returnNone, div2IfNotOdd)
-    println(applyTransformations(7, seq))
+    seq = Seq( add1IfOdd, returnNone, div2IfNotOdd)
+    println(applyTransformationsByReducing(7, seq))
+    println(applyTransformationsByPatternMatching(7, seq))
   }
+
+  def applyTransformationsByReducing(i: Int, transformations: Seq[(Int) => Option[Int]]): Int = {
+    if(transformations.isEmpty) i
+    else transformations.reduceRight(
+      (funcRight, funcLeft) =>
+      {
+        number => funcLeft.apply(funcRight.apply(number).getOrElse(number))
+      }
+    )
+      .apply(i)
+      .getOrElse(i)
+  }
+
+  def applyTransformationsByPatternMatching(i: Int, transformations: Seq[(Int) => Option[Int]]): Int =
+    transformations match {
+      case Nil => i
+      case head :: tail => applyTransformationsByPatternMatching(head.apply(i).getOrElse(i), tail)
+    }
 
   def div2IfNotOdd(i: Int): Option[Int] = if(i % 2 > 0) None else Some(i / 2)
 
   def add1IfOdd(i: Int): Option[Int] = if(i % 2 > 0) Some(i + 1) else Some(i)
 
   def returnNone(i: Int): Option[Int] = None
-
-  def applyTransformations(i: Int, transformations: Seq[(Int) => Option[Int]]): Int = {
-    transformations.reduceRight(
-      (funcRight, funcLeft) =>
-        {
-          number => funcLeft.apply(funcRight.apply(number).getOrElse(number))
-        }
-    )
-      .apply(i)
-      .getOrElse(i)
-    /*transformations match {
-      case Nil => i
-      case head :: tail => applyTransformations(head.apply(i).getOrElse(i), tail)
-    }*/
-  }
 }
