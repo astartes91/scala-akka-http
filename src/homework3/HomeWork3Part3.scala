0117aefb723edb5174be942255df5b89ee3ec75b
unknown <astartes91@gmail.com>
2018-03-09 14:14:20 +0700

    HomeWork 3 part 3 changed

--- src/homework3/HomeWork3Part3.scala
+++ src/homework3/HomeWork3Part3.scala
@@ -8,26 +8,32 @@ object HomeWork3Part3 {
   def main(args: Array[String]) {
 
     var seq: Seq[(Int) => Option[Int]] = Seq(div2IfNotOdd, add1IfOdd)
-    println(applyTransformationsByReducing(10, seq))
-    println(applyTransformationsByPatternMatching(10, seq))
+    /*println(applyTransformationsByReducing(10, seq))
+    println(applyTransformationsByPatternMatching(10, seq))*/
 
-    seq = Seq( add1IfOdd, returnNone, div2IfNotOdd)
-    println(applyTransformationsByReducing(7, seq))
+    seq = Seq(add1IfOdd, returnNone, div2IfNotOdd)
+    //println(applyTransformationsByReducing(7, seq))
     println(applyTransformationsByPatternMatching(7, seq))
   }
 
-  def applyTransformationsByReducing(i: Int, transformations: Seq[(Int) => Option[Int]]): Int = {
+  /*def applyTransformationsByReducing(i: Int, transformations: Seq[(Int) => Option[Int]]): Int = {
     if(transformations.isEmpty) i
-    else transformations.reduceRight(
-      (funcRight, funcLeft) =>
+    else transformations.reduce(
+      (funcLeft, funcRight) =>
       {
-        number => funcLeft(funcRight(number).getOrElse(number))
-        //number => funcRight.andThen(funcRightResult => funcLeft(funcRightResult.getOrElse(number))).apply(number)
+        number =>
+          println(s"new function arg: $number")
+          val funcLeftRes: Int = funcLeft(number).getOrElse(number)
+          println(s"funcLeftRes: $funcLeftRes")
+          val funcRightRes: Option[Int] = funcRight(funcLeftRes)
+          println(s"funcRightRes: $funcRightRes")
+            funcRightRes
+        //number => funcLeft.andThen(funcLeftResult => funcRight(funcLeftResult.getOrElse(number))).apply(number)
       }
     )
       .apply(i)
       .getOrElse(i)
-  }
+  }*/
 
   def applyTransformationsByPatternMatching(i: Int, transformations: Seq[(Int) => Option[Int]]): Int =
     transformations match {
@@ -35,9 +41,24 @@ object HomeWork3Part3 {
       case head :: tail => applyTransformationsByPatternMatching(head(i).getOrElse(i), tail)
     }
 
-  def div2IfNotOdd(i: Int): Option[Int] = if(i % 2 > 0) None else Some(i / 2)
+  def div2IfNotOdd(i: Int): Option[Int] = {
+    println(s"div2IfNotOdd called with arg $i")
+    val res: Option[Int] = if(i % 2 > 0) None else Some(i / 2)
+    println(s"div2IfNotOdd return $res")
+    res
+  }
 
-  def add1IfOdd(i: Int): Option[Int] = if(i % 2 > 0) Some(i + 1) else Some(i)
+  def add1IfOdd(i: Int): Option[Int] = {
+    println(s"add1IfOdd called with arg $i")
+    val res: Some[Int] = if(i % 2 > 0) Some(i + 1) else Some(i)
+    println(s"add1IfOdd return $res")
+    res
+  }
 
-  def returnNone(i: Int): Option[Int] = None
+  def returnNone(i: Int): Option[Int] = {
+    println(s"returnNone called with arg $i")
+    val res: None.type = None
+    println(s"returnNone return $res")
+    res
+  }
 }
